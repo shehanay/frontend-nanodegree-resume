a7d5bb81ccce5be5958c43baaf3bb880e987d7a5
shehanay <shehanay@hotmail.co.uk>
2017-09-15 17:01:23 +0300

    my resume
    my final changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,27 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- console.log(" misk");
+var name="Shehana Alsubaiy";
+var role="Web Devloper";
+var mobile="099-555-5555";
+var email="Shehana@example.com";
+var github="Shehanay";
+var twitter="@shehanay";
+var location="Riyadh";
+
+ var formattedName = HTMLheaderName.replace("%data%", name );
+ $("#header").append(formattedName);
+ var formattedRole= HTMLheaderRole.replace("%data%", role);
+ $("#header").append(formattedRole);
+
+ var work={
+ 	job:[{employer:"Shehana", title:"Teaching"},{employer:"Shehana",title:"Trainer at KKESH Hospital"}]
+ 	};
+ for(jobs in work.job){
+ 	$("#workExperience").append(HTMLworkStart);
+var formattedEmployer= HTMLworkEmployer.replace("%data%",work.job[jobs].employer);
+var formattedTitle= HTMLworkTitle.replace("%data%",work.job[jobs].title);
+var formattedEmployerTitle=formattedEmployer + formattedTitle;
+
+ $(".work-entry:last").append(formattedEmployerTitle);
+ }
