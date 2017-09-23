4da09ff8e4c81017adda2bece7cc504a2671b1a6
shehanay <shehanay@hotmail.co.uk>
2017-09-24 02:18:04 +0300

    add education, work and projects

--- js/helper.js
+++ js/helper.js
@@ -58,7 +58,6 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
- 
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
@@ -238,12 +237,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,25 +3,159 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 var name="Shehana Alsubaiy";
 var role="Web Devloper";
-var mobile="099-555-5555";
-var email="Shehana@example.com";
-var github="Shehanay";
-var twitter="@shehanay";
-var location="Riyadh";
+var contacts={
+ mobile:"099-555-5555",
+ email:"Shehana@example.com",
+ github:"Shehanay",
+ twitter:"@shehanay",
+ location:"Riyadh",
+ welcomeMessage:"Hello, This is my resume :)",
+ skills:["Programmer","Quick Learner"],
+ biopic:"images/fry.jpg"
 
- var formattedName = HTMLheaderName.replace("%data%", name );
- $("#header").append(formattedName);
- var formattedRole= HTMLheaderRole.replace("%data%", role);
- $("#header").append(formattedRole);
+};
 
+ function displaySkills (a){
+ 	$("#topContacts").append(HTMLskillsStart);
+for (var i = 0; i < a.length; i++)  {
+var formattedSkills= HTMLskills.replace("%data%", a[i]);
+$("#skills").append(formattedSkills);
+                             }
+                           }
+ 
  var work={
- 	job:[{employer:"Shehana", title:"Teaching"},{employer:"Shehana",title:"Trainer at KKESH Hospital"}]
+ 	job:[
+ 	     {
+ 	employer:"Shehana", 
+ 	title:"Teaching", 
+ 	location:"Riyadh", 
+ 	dates:"2016", 
+ 	description:"I teached girls who didn't specialized at universities a simple course of a computer"
+         },
+         {
+    employer:"Shehana",
+    title:"Trainer at KKESH Hospital",
+    location:"Riyadh", 
+ 	dates:"2017", 
+ 	description:"I took my training at KKESH Hospital to help doctors and their assistants through their work"
+        
+          }
+          ]
  	};
- for(jobs in work.job){
- 	$("#workExperience").append(HTMLworkStart);
-var formattedEmployer= HTMLworkEmployer.replace("%data%",work.job[jobs].employer);
-var formattedTitle= HTMLworkTitle.replace("%data%",work.job[jobs].title);
-var formattedEmployerTitle=formattedEmployer + formattedTitle;
 
- $(".work-entry:last").append(formattedEmployerTitle);
- }
+ 	function displayWork (a){
+ 	$("#workExperience").append(HTMLworkStart);
+for (var i = 0; i < a.length; i++)  {
+var formattedEmployer= HTMLworkEmployer.replace("%data%",a[i].employer);
+var formattedTitle= HTMLworkTitle.replace("%data%",a[i].title);
+var formattedLocation=HTMLworkLocation.replace("%data%",a[i].location);
+var formattedDate=HTMLworkDates.replace("%data%",a[i].dates);
+var formattedDescription=HTMLworkDescription.replace("%data%",a[i].description);
+var formattedWork=formattedEmployer + formattedTitle + formattedLocation + formattedDate + formattedDescription;
+ $(".work-entry:last").append(formattedWork);
+                             }
+                           }
+
+var project={
+projects:[
+{
+	title:"Solving Edge Matching with SAT Solver ",
+	dates:"2017",
+	description:"This project is my BSc project and it about solving edge matching game with SAT solver ",
+	images:"images/EM puzzle unbounded unsined.png"
+}
+         ]
+}
+
+function displayProject (a){
+ 	$("#projects").append(HTMLprojectStart);
+for (var i = 0; i < a.length; i++)  {
+var formattedTitle= HTMLprojectTitle.replace("%data%",a[i].title);
+var formattedDate=HTMLprojectDates.replace("%data%",a[i].dates);
+var formattedDescription=HTMLprojectDescription.replace("%data%",a[i].description);
+var formattedImages=HTMLprojectImage.replace("%data%",a[i].images);
+var formattedProject= formattedTitle + formattedDate + formattedDescription + formattedImages;
+ $(".project-entry:last").append(formattedProject);
+                                     }
+                          }
+
+var education={
+	schools:[
+	             {
+             name:"Alabna'a Schools",
+             location:"Riyadh",
+             degree:"high school",
+             majors:["secondary school"],
+             dates:"2012"      
+	             }
+	         ],
+	 onlineCourses:[
+	                  {
+	 	     title:"Algorithm Course",
+	 	     school:"Coursera",
+	 	     dates:"2016",
+             url:"https://www.coursera.org"
+	                   }    
+	               ]    
+}
+ 
+function displayEducation (a){
+$("#education").append(HTMLschoolStart);
+
+$("#education").append(HTMLonlineClasses);
+
+for (var i = 0; i < a.schools.length; i++)  {
+var formattedName=HTMLschoolName.replace("%data%",a.schools[i].name);
+var formattedDegree=HTMLschoolDegree.replace("%data%",a.schools[i].degree);
+var formattedDate=HTMLschoolDates.replace("%data%",a.schools[i].dates);
+var formattedLocation=HTMLschoolLocation.replace("%data%",a.schools[i].location);
+var formattedMajor=HTMLschoolMajor.replace("%data%",a.schools[i].majors[i]);                           
+var formattedSchool= formattedName + formattedDegree + formattedDate + formattedLocation  + formattedMajor;
+$(".education-entry:last ").append(formattedSchool);
+                                             }
+
+for (var j = 0; j < a.onlineCourses.length; j++)  {
+var formattedTitle=HTMLonlineTitle.replace("%data%",a.onlineCourses[j].title);
+var formattedSchool=HTMLonlineSchool.replace("%data%",a.onlineCourses[j].school);
+var formattedDates=HTMLonlineDates.replace("%data%",a.onlineCourses[j].dates);
+var formattedUrl=HTMLonlineURL.replace("%data%",a.onlineCourses[j].url);                      
+var formattedOnlineCourses= formattedTitle + formattedSchool + formattedDates + formattedUrl  ;
+$(".education-entry:last ").append(formattedOnlineCourses);
+                                             }
+ 
+
+                               } 
+ 
+
+ var formattedRole= HTMLheaderRole.replace("%data%", role);
+ $("#header").prepend(formattedRole);
+var formattedName = HTMLheaderName.replace("%data%", name );
+ $("#header").prepend(formattedName);
+ 
+var formattedMobile= HTMLmobile.replace("%data%", contacts.mobile);
+ $("#topContacts").prepend(formattedMobile);
+var formattedEmail= HTMLemail.replace("%data%", contacts.email);
+ $("#topContacts").append(formattedEmail);
+ var formattedGithub= HTMLgithub.replace("%data%", contacts.github);
+ $("#topContacts").append(formattedGithub);
+ var formattedTwitter= HTMLtwitter.replace("%data%", contacts.twitter);
+ $("#topContacts").append(formattedTwitter);
+ var formattedLocation= HTMLlocation.replace("%data%", contacts.location);
+ $("#topContacts").append(formattedLocation);
+ var formattedBiopic= HTMLbioPic.replace("%data%", contacts.biopic);
+ $("#topContacts").append(formattedBiopic);
+ var formattedWelcomeMessage= HTMLwelcomeMsg.replace("%data%", contacts.welcomeMessage);
+ $("#topContacts").append(formattedWelcomeMessage);
+ displaySkills(contacts.skills);
+ displayWork (work.job);
+ displayProject (project.projects);
+displayEducation(education);
+var mapString=" https://www.google.com.sa/maps/@26.6726653,42.7608445,4.27z?hl=ar";
+var formattedMap=googleMap.replace("%data%", mapString) ;
+$("#mapDiv").append(googleMap);
+
+ $("#footerContacts").prepend(formattedMobile);
+ $("#footerContacts").append(formattedEmail);
+ $("#footerContacts").append(formattedGithub);
+ $("#footerContacts").append(formattedTwitter);
+ $("#footerContacts").append(formattedLocation);
